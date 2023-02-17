0811baad79e3fb6d247c5ae0c5c56c3a513f3f14
Eugene Kuzenko <yevgenkuzenko@gmail.com>
2023-02-17 22:04:14 +0200

    Time Zomes Final Version

--- src/componets/Timer.tsx
+++ src/componets/Timer.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 
 import timeZones from "../time-zones";
@@ -8,16 +7,10 @@ type TimerProps = {
 }
 export const Timer: React.FC<TimerProps> = (props) => {
     const timeZoneIndex: number =
-     timeZones.findIndex(tz => JSON.stringify(tz).includes(props.cityOrCountry));
+     timeZones.findIndex(tz => JSON.stringify(tz).toLowerCase().includes(props.cityOrCountry.toLowerCase()));
     const [timeZone, setTimeZone] = React.useState(timeZones[timeZoneIndex]?.name);
-    // with UseState
-    //const [timeZoneName, setTimeZoneName] = React.useState(timeZone ?
-    //     props.cityOrCountry : "Israel");
-
-    // with useRef
-    const timeZoneName = React.useRef(timeZone ?
-            props.cityOrCountry : "Israel");
-
+    let timeZoneName = React.useRef (timeZone ?
+         props.cityOrCountry : "Israel");
     const [time, setTime] = React.useState(new Date());
     function tick() {
         console.log("tick");
@@ -25,8 +18,13 @@ export const Timer: React.FC<TimerProps> = (props) => {
     }
     React.useEffect(()=>{
         const interval = setInterval(tick, 1000);
+        const timeZoneIndex: number =
+     timeZones.findIndex(tz => JSON.stringify(tz).includes(props.cityOrCountry));
+     timeZoneName.current = timeZone ?
+         props.cityOrCountry : "Israel"
+     setTimeZone(timeZones[timeZoneIndex]?.name)
         return ()=>clearInterval(interval);
-    }, [])
+    }, [props])
     
     function processCityCountry(value: string): string {
         const index =  timeZones.findIndex(tz => JSON.stringify(tz).includes(value));
@@ -34,19 +32,14 @@ export const Timer: React.FC<TimerProps> = (props) => {
         if (index < 0) {
             res = `${value} is wrong city / country, please type again`;
         } else {
-            // with useState
-         //   setTimeZoneName(value);
-
-         // with useRef
-         timeZoneName.current = value;
+            timeZoneName.current = value;
             setTimeZone(timeZones[index].name);
         }
         return res;
     }
-  // .current for useRef
+
     return <div>
         <Input placeHolder={"Enter city or country"} inputProcess={processCityCountry}/>
-     
         <h3 className='logo'>Time in {timeZoneName.current}</h3>
         <label style={{display: "block",
          textAlign: "center", fontSize: "2em"}}>
